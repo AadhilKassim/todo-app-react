abc1d98a3326a9eda88999532f70ba3e6c14fad0
Aadhil Kassim <aadhikassim@gmail.com>
2025-06-17 16:01:03 +0530

    Refactored the whole front end
    The home is blank and has a navbar. Login takes email and password. Signup takes name, email, and password. Made the Loginform the components that handles the whole login process instead of it being a printer to print the props

--- components/form/Loginform.jsx
+++ components/form/Loginform.jsx
@@ -1,14 +1,49 @@
+import Login from '@/pages/Login';
+import axios from 'axios';
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 
 const Loginform = (props) => {
-  
-  console.log(props)
-    return (
+
+  const [psswrd, setPsswrd] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  console.log("Email:", email);
+
+
+  return (
     <>
-        <h1>Login Form</h1>
-        <p> {props.userName}  {props.userPss}</p>
+      <div>
+        <h1 align="center">Login</h1>
+      </div>
+      <form>
+
+      <div>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email" />
+      </div>
+      <br></br>
+      <div>
+        <input
+          type="password"
+          onChange={(e) => setPsswrd(e.target.value)}
+          placeholder="Enter Password" />
+      </div>
+      <button type='Submit' onClick={() => {
+        if (psswrd.length < 8) {
+          alert("Password must be at least 8 characters long");
+        } else {
+          alert(`Welcome ${name}, you have successfully signed up!`);
+        }
+      }}>Sign Up</button>
+      </form>
+
     </>
-  )
+  );
 }
 
 export default Loginform
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,21 @@
-import Loginform from '@/components/form/Loginform'
-import React from 'react'
+import Head from "next/head";
+import React from "react";
+import Link from "next/link";
 
-const index = () => {
+export default function Home() {
   return (
     <>
-        <Loginform /> 
+      <Head>
+        <title>Home Page | Todo App</title>
+        <meta name="Home page" content="Simple No login page for user browsing" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div>
+        <Link href="/">Home</Link> |{" "}
+        <Link href="/Login">Login</Link> |{" "}
+        <Link href="/signup">Sign Up</Link>
+      </div>
     </>
-  )
+  );
 }
-
-export default index
